8bde4e23b6d60c7c8f8457235fc8d9330992fa1d
aofleejay <aofleejay@hotmail.com>
2018-11-20 23:29:03 +0700

    :construct: Add construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
+  <div className="container">
+    <div className="background">
+      <p className="text">
+        UNDER CONSTRUCTION
       </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    </div>
   </div>
 )
 
