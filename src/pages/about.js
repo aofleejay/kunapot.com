27716c16b4aab5d53b073741f3ab10ff0b2cc649
aofleejay <aofleejay@hotmail.com>
2019-02-18 20:16:51 +0700

    :rotating_light: Wrap emoji inside span.

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const About = () => (
     <SEO />
     <h1>About</h1> 
     <h2>Kunapot Pairat</h2>
-    <p>💻 Software Developer / 🏃 Slow Runner / 🐱 Pokemon Trainer / 😻 Cat Lover / 🎸 Guitar Player</p>
+    <p><span role="img" aria-label="computer">💻</span> Software Developer / <span role="img" aria-label="runner">🏃</span> Slow Runner / <span role="img" aria-label="cat">🐱</span> Pokemon Trainer / <span role="img" aria-label="lovely-cat">😻</span> Cat Lover / <span role="img" aria-label="guitar">🎸</span> Guitar Player</p>
     <p>Medium - <a href="https://medium.com/@aofleejay">https://medium.com/@aofleejay</a></p>
     <p>Github - <a href="https://github.com/aofleejay">https://github.com/aofleejay</a></p>
   </Layout>
