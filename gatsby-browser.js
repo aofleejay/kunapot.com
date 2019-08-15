cec72243fbbe409d004f7f4c5145f0f4d8580167
คุณาพจน์ ไพรัตน์ <aofleejay@hotmail.com>
2019-08-15 23:18:34 +0700

    :bug: Add default theme.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import React from 'react'
 import themeContext from './src/context/theme'
 import './src/styles/global.css'
 
-const theme = localStorage.getItem('theme')
+const theme = localStorage.getItem('theme') || 'light'
 
 export const wrapRootElement = ({ element }) => {
   return <themeContext.Provider value={theme}>{element}</themeContext.Provider>
