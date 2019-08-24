3cb449fb4919493b9045702aa2b2d74c423a8681
Kunapot Pairat <aofleejay@hotmail.com>
2019-08-24 21:30:52 +0700

    :sparkles: Change image max width. Support webp.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,8 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 700,
+              withWebp: true,
             },
           },
           {
