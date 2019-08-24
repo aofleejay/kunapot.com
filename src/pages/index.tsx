3f6a884c6749f2dd19748b81cab2577419437285
Kunapot Pairat <aofleejay@hotmail.com>
2019-08-25 00:35:16 +0700

    :sparkles: Add pagination for blogs and tags page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { css } from '@emotion/core'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { rhythm } from '../utils/typography'
+import BlogCard from '../components/BlogCard'
 
 interface AllMarkdownProps {
   data: {
@@ -36,69 +35,7 @@ const IndexPage = (props: AllMarkdownProps) => (
   <Layout>
     <SEO />
     {props.data.allMarkdownRemark.edges.map(({ node }) => (
-      <div
-        key={node.id}
-        css={css`
-          margin-bottom: ${rhythm(2)};
-        `}
-      >
-        <Link
-          css={css`
-            text-decoration: none;
-            color: inherit;
-          `}
-          to={node.fields.slug}
-        >
-          <h1
-            css={css`
-              margin-bottom: ${rhythm(1 / 2)};
-            `}
-          >
-            {node.frontmatter.title}
-          </h1>
-        </Link>
-        <p
-          css={css`
-            color: #bbb;
-          `}
-        >
-          {node.frontmatter.date}
-          <span
-            css={css`
-              padding: 0 ${rhythm(1 / 2)};
-            `}
-          >
-            |
-          </span>
-          {node.frontmatter.tags.map(tag => (
-            <Link
-              key={tag}
-              to={`/tags/${tag}`}
-              css={css`
-                text-decoration: none;
-                color: inherit;
-              `}
-            >
-              <span>{tag}</span>
-            </Link>
-          ))}
-        </p>
-        <Link
-          css={css`
-            text-decoration: none;
-            color: inherit;
-          `}
-          to={node.fields.slug}
-        >
-          {node.frontmatter.cover && (
-            <img
-              src={node.frontmatter.cover.publicURL}
-              alt={node.frontmatter.cover.name}
-            />
-          )}
-          <p>{node.excerpt}</p>
-        </Link>
-      </div>
+      <BlogCard post={node} />
     ))}
   </Layout>
 )
