64c113acee6d3c74b27b491837b6f3ab7f38a047
aofleejay <>
2019-12-20 23:02:45 +0700

    :rotating_light: Add cypress eslint plugin.

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,5 +1,7 @@
 it('Home page is working.', () => {
   cy.visit('/')
 
-  cy.contains('Home').should('to.visible')
+  cy.contains("Books I've read").should('to.visible')
+  cy.contains("Blogs I've written").should('to.visible')
+  cy.contains("Games I've played").should('to.visible')
 })
