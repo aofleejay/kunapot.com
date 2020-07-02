8a9550ffbcf672f46fd60512fbe26839e0b6503e
aofleejay <aofleejay@hotmail.com>
2020-07-03 03:20:23 +0700

    :sparkles: Add resize image script.

---
+++ src/scripts/resize-image.ts
@@ -0,0 +1,52 @@
+import inquirer from 'inquirer'
+import sharp from 'sharp'
+
+enum ImageType {
+  coverImage = 'cover image',
+  bookCover = 'book cover',
+}
+
+type Input = {
+  imageType: ImageType
+  inputFile: string
+  outputFolder: string
+}
+
+const resize = ({ imageType, inputFile, outputFolder }: Input) => {
+  const imageSize = [
+    { name: ImageType.coverImage, width: 1600, height: 900 },
+    { name: ImageType.bookCover, width: 900, height: 1300 },
+  ]
+  const size = imageSize.find(size => size.name === imageType)
+
+  if (size) {
+    sharp(inputFile)
+      .resize(size.width, size.height)
+      .jpeg()
+      .toFile(`${outputFolder}/foo-cover-image.jpg`)
+  }
+}
+
+inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'imageType',
+      message: 'Select image type ?',
+      choices: [ImageType.coverImage, ImageType.bookCover],
+    },
+    {
+      type: 'input',
+      name: 'inputFile',
+      message: 'Drag input file here ?',
+      validate: title => !!title || 'Enter input file bro!!',
+    },
+    {
+      type: 'input',
+      name: 'outputFolder',
+      message: 'Drag output folder here ?',
+      validate: title => !!title || 'Enter output folder bro!!',
+    },
+  ])
+  .then(answers => resize(answers))
+  .catch(error => console.error(error))
